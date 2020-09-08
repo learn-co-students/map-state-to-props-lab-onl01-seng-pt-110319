552d816d70a2c7a87aaf763f387c12358723b335
Carlumbo <Carlpalumbo1@gmail.com>
2020-09-08 11:57:24 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-//import { reporters } from 'mocha';
 
 class Users extends Component {
-
   render() {
-  let users = this.props.users.map((user, index ) => <li key={index}>{user.username}</li>)
-    return (
-      <div>
-        {this.props.numOfUsers}
-        <ul>
-          {users}
-        </ul>
-      </div>
-    )
+  let users = this.props.users.map(( users, index) => <li key={index}>{users.username}</li>)
+
+  return (
+    <div>
+      {this.props.numOfUsers}
+    <ul>
+    {users}
+    </ul>
+    
+    </div>
+   )
   }
 }
 
-//add mapStateToProps here
 const mapStateToProps = (state) => {
   return {
-    users: state.users,
+    users: state.users, 
     numOfUsers: state.users.length
-  }
-}
+  };
+};
 
-// connect this component to Redux
-export default connect(mapStateToProps)(Users)
+export default connect(mapStateToProps)(Users);
